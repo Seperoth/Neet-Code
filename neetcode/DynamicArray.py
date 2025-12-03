d32d02da77fd4d817b404b5da93fbb0bc7e56a00
Seperoth <152127758+Seperoth@users.noreply.github.com>
2025-12-03 19:13:49 +0800

    Update DynamicArray.py

--- neetcode/DynamicArray.py
+++ neetcode/DynamicArray.py
@@ -4,7 +4,11 @@ class DynamicArray:
         self.capacity = capacity
         self.size = 0
         self.array = [None] * capacity
-
+        
+    def get(self, i: int) -> int:
+        if i < 0 or i >= self.size:
+            raise IndexError("Index out of bounds")
+        return self.array[i]
 
 arr = DynamicArray(5)
 
@@ -13,5 +17,3 @@ print("Capacity: ", arr.capacity)
 print("Size: ", arr.size)
 
 print("Internal Array: ", arr.array)
-
-print("Adding elements to the dynamic array...")
