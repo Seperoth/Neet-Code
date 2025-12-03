d69eb317609beadf43e6c2350d6fb0732705c63c
Seperoth <152127758+Seperoth@users.noreply.github.com>
2025-12-03 19:07:40 +0800

    Add files via upload

---
+++ neetcode/DynamicArray.py
@@ -0,0 +1,15 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.array = [None] * capacity
+
+
+arr = DynamicArray(5)
+
+print("Capacity: ", arr.capacity)
+
+print("Size: ", arr.size)
+
+print("Internal Array: ", arr.array)
